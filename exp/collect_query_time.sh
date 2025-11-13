aef0e8fb9407099f2b0ad09fe2820a4b7e6e83e9
111108s <abb10000y@gmail.com>
2025-11-13 11:02:17 +0800

    improve writting

--- exp/collect_query_time.sh
+++ exp/collect_query_time.sh
@@ -59,7 +59,7 @@ parse_result_file() {
 
 outputFile="[summary]query_time"
 touch ${outputFile}
-echo -e "xbwt_time(us)\ttime(us)\tocc\ttext_length\ttype\tcorpus\twith_mf\ttau_l\ttau_u\tlambda\tmasked_ratio" > "${outputFile}"
+echo -e "xbwt_time(us)\total_ttime(us)\tocc\ttext_length\ttype\tcorpus\twith_mf\ttau_l\ttau_u\tlambda\tmasked_ratio" > "${outputFile}"
 
 for config in "${configs[@]}"; do
   eval set -- $config
